da5c85c1a558d90c9c62e7f96b716f8797d5df8e
GubbinLubbin <silwalankit27@gmail.com>
2026-01-03 16:16:15 +0545

    Factors and GCd(HCF) and LCM math II

---
+++ src/com.mathsII/factorOfNumber.java
@@ -0,0 +1,17 @@
+public class factorOfNumber {
+  public static void main(String[] args) {
+     factors(40);
+  }
+  static void factors(int n){
+    for(int i=1;i<=Math.sqrt(n);i++){
+      if(n%i==0){
+        if(n%i==i){
+          System.out.print(i+" ");
+        }else{
+          System.out.print(i+" "+n/i+" ");
+        }
+      }
+    }
+    return;
+  }
+}
---
+++ src/com.mathsII/gcdAndLcm.java
@@ -0,0 +1,16 @@
+public class gcdAndLcm {
+  public static void main(String[] args) {
+      System.out.println(gcd(55,57));
+      System.out.println(lcm(9, 18));
+  }
+  static int gcd(int a,int b){
+    if(a==0){
+      return b;
+    }
+    return gcd(b%a,a);
+  }
+  static int lcm(int a,int b){
+    int d=gcd(a,b);
+    return (a/d)*(b/d)*d;
+  }
+}
