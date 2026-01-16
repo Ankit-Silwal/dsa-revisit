6e54b702e5e971ff2fc0faa1a421690ef98a4604
GubbinLubbin <silwalankit27@gmail.com>
2026-01-16 23:09:57 +0530

    BST tree basics

--- src/BInaryTrees/BinaryImplemenation.java
+++ src/BInaryTrees/BinaryImplemenation.java
@@ -1,11 +1,13 @@
 package BInaryTrees;
-
-import java.util.Scanner;
 public class BinaryImplemenation {
   public static void main(String[] args) {
-      Scanner scanner=new Scanner(System.in);
-      BinaryTree tree=new BinaryTree();
-      tree.populate(scanner);
-      tree.display();
+      // Scanner scanner=new Scanner(System.in);
+      // BinaryTree tree=new BinaryTree();
+      // tree.populate(scanner);
+      // tree.display();
+    BST tree=new BST();
+    int[] nums={1,2,3,4,5,6,7,8,9,10};
+    tree.populate(nums);
+    tree.display();
   }
 }
